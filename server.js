bfea5d8fb66a9da48f3c6422a2fae9173a514f69
Alexander Harding <alexander.harding@johnsonfit.com>
2015-12-11 18:58:09 -0600

    again...

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.get('/', function (req, res) {
   });
 });
 
-var server = app.listen(8080, function () {
+var server = app.listen(process.env.OPENSHIFT_NODEJS_PORT || 8080, process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1", function () {
   var host = server.address().address;
   var port = server.address().port;
 
